a82285609a51590995304135c6b0aa1a54823479
jiuc <zm.ptz.ru2010@yandex.ru>
2022-06-20 13:06:32 +0500

    Настройка консольного приложения Рефакторинг WPF

---
+++ UI/WeatherWebSolution.ConsoleUI/WeatherWebSolution.ConsoleUI/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace WeatherWebSolution.ConsoleUI
+{
+    internal class Program
+    {
+        private static IHost __Hosting;
+
+        public static IHost Hosting => __Hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        private static IHostBuilder CreateHostBuilder(string[] args) => Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(ConfigureServices);
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            using var host = Hosting;
+            await host.StartAsync();
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+            await host.StopAsync();
+
+        }
+    }
+}
