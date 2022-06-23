813fc0cd45dfb9f527e54e9ab5e21a18b73bbd48
jiuc <zm.ptz.ru2010@yandex.ru>
2022-06-23 11:00:36 +0500

    Первый метод контроллера в API

---
+++ Services/WeatherWebSolution.API/API/Controllers/DataSourcesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WeatherWebSolution.DAL.Entities;
+using WeatherWebSolution.Intefaces.Base.Entities.Reposytories;
+
+namespace WeatherWebSolution.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataSourcesController : ControllerBase
+    {
+        private readonly IRepository<DataSource> _Repository;
+
+        public DataSourcesController(IRepository<DataSource> Repository)
+        {
+            _Repository = Repository;
+        }
+
+        [HttpGet("count")]
+        public async Task<IActionResult> GetItemsCount() => Ok(await _Repository.GetCount());
+    }
+}
