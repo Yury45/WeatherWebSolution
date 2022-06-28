260c80d44d5f0820e44a43dcf6420c9fce9baeb9
jiuc <zm.ptz.ru2010@yandex.ru>
2022-06-28 11:11:04 +0500

    Выделение абстрактного контроллера, рефакторинг

--- Services/WeatherWebSolution.API/API/Controllers/DataSourcesController.cs
+++ Services/WeatherWebSolution.API/API/Controllers/DataSourcesController.cs
@@ -9,108 +9,21 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.DependencyInjection;
+using WeatherWebSolution.API.Controllers.Base;
 using WeatherWebSolution.DAL.Context;
 using WeatherWebSolution.DAL.Entities;
 using WeatherWebSolution.Intefaces.Base.Entities.Reposytories;
 
 namespace WeatherWebSolution.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DataSourcesController : ControllerBase
+    public class DataSourcesController : EntityController<DataSource>
     {
         private readonly IRepository<DataSource> _Repository;
 
 
-        public DataSourcesController(IRepository<DataSource> Repository)
+        public DataSourcesController(IRepository<DataSource> repository) : base(repository)
         {
-            _Repository = Repository;
-        }
 
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        public async Task<IActionResult> Add(DataSource item)
-        {
-            var result = await _Repository.Add(item).ConfigureAwait(false);
-            return AcceptedAtAction(nameof(Get), new { id = result.Id }, result);
-
-        }
-
-        [HttpGet("page/{pageIndex:int}/{pageSize:int}")]
-        [HttpGet("page/[[{pageIndex:int}/{pageSize:int}]]")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<IPage<DataSource>>> GetPage(int pageIndex, int pageSize)
-        {
-            var result = await _Repository.GetPage(pageIndex, pageSize);
-            return result.Items.Any()
-                ? Ok(result)
-                : NotFound(result);
-        }
-
-        [HttpGet("count")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(int))]
-        public async Task<IActionResult> GetItemsCount() => Ok(await _Repository.GetCount());
-
-        [HttpPost("exist")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(bool))]
-        public  async Task<IActionResult> Exist(DataSource item) => await _Repository.Exist(item) ? Ok(true) : NotFound(false);
-
-        [HttpGet("exist/id{id:int}")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
-        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(bool))]
-        public async  Task<IActionResult> ExistId(int id) => await _Repository.ExistId(id) ? Ok(true) : NotFound(false);
-
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetAll() => Ok(await _Repository.GetAll());
-
-        [HttpGet("{id:int}")]
-        [ActionName("Get")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Get(int id)
-        {
-            var item = await _Repository.GetById(id);
-            if(item == null)
-                return NotFound();
-            return Ok(item);
-        }
-
-        [HttpGet("items[[{skip:int}-{count:int}]]")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<IEnumerable<DataSource>>>Get(int skip, int count) =>
-        Ok(await _Repository.Get(skip, count));
-
-        [HttpPut]
-        [ProducesResponseType(StatusCodes.Status202Accepted)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Update(DataSource item)
-        {
-            if (await _Repository.Update(item) is not { } result)
-                return NotFound(item);
-            return AcceptedAtAction(nameof(Get), new { id = result.Id }, result);
-        }
-
-        [HttpDelete]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Delete(DataSource item)
-        {
-            if(await _Repository.Delete(item) is not { } result)
-                return NotFound(item);
-            return Ok(result);
-        }
-
-        [HttpDelete("{id:int}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> DeleteById(int id)
-        {
-            if(await _Repository.DeleteById(id) is not { } result)
-                return NotFound(id);
-            return Ok(result);
         }
 
     }
