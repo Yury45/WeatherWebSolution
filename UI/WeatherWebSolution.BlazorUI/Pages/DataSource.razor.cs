e28f0b20283b305f8a8be8f01c00fc0e0ea3b111
jiuc <zm.ptz.ru2010@yandex.ru>
2022-07-08 15:55:31 +0500

    Добавлена кнопка и функция удаления в макете

--- UI/WeatherWebSolution.BlazorUI/Pages/DataSource.razor.cs
+++ UI/WeatherWebSolution.BlazorUI/Pages/DataSource.razor.cs
@@ -4,10 +4,11 @@ namespace WeatherWebSolution.BlazorUI.Pages
 {
     public partial class DataSource
     {
+        private DataSourceInfo _createdNewSouceModel = new ();
+
         private IList<DataSourceInfo> _sources;
 
         private async Task UpdateSource() => _sources = (await _repository.GetAll()).ToList();
-
 
         protected override async Task OnInitializedAsync() => await UpdateSource();
 
@@ -19,6 +20,14 @@ namespace WeatherWebSolution.BlazorUI.Pages
 
             var removed = await _repository.Delete(source);
             if (removed is not null) _sources.Remove(removed);
+        }
+
+        private async Task OnSourceCreated()
+        {
+            var created_source = await _repository.Add(_createdNewSouceModel);
+            if(created_source is null) return;
+            _sources.Add(created_source);
+            _createdNewSouceModel = new();
         }
     }
 }
