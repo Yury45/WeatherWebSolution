3a77aefeef75940bdd6f2c45f33eacdfd033dece
jiuc <zm.ptz.ru2010@yandex.ru>
2022-07-08 15:34:24 +0500

    Добавлен метод удаления из базы данных

--- Common/WeatherWebSolution.Domain.Base/DataSourceInfo.cs
+++ Common/WeatherWebSolution.Domain.Base/DataSourceInfo.cs
@@ -1,13 +1,49 @@
 ﻿
+using System;
 using WeatherWebSolution.Intefaces.Base.Entities;
 
 namespace WeatherWebSolution.Domain.Base
 {
-    public class DataSourceInfo : INamedEntity
+    public class DataSourceInfo : INamedEntity, IEquatable<DataSourceInfo>
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
+        public bool Equals(DataSourceInfo other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Id == other.Id && Name == other.Name && Description == other.Description;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((DataSourceInfo)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Id;
+                hashCode = (hashCode * 397) ^ (Name != null ? Name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Description != null ? Description.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(DataSourceInfo left, DataSourceInfo right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(DataSourceInfo left, DataSourceInfo right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
---
+++ UI/WeatherWebSolution.BlazorUI/Pages/DataSource.razor.cs
@@ -0,0 +1,24 @@
+﻿using WeatherWebSolution.Domain.Base;
+
+namespace WeatherWebSolution.BlazorUI.Pages
+{
+    public partial class DataSource
+    {
+        private IList<DataSourceInfo> _sources;
+
+        private async Task UpdateSource() => _sources = (await _repository.GetAll()).ToList();
+
+
+        protected override async Task OnInitializedAsync() => await UpdateSource();
+
+        private async Task RefreshButton_OnClick() => await UpdateSource();
+
+        private async Task Remove(DataSourceInfo source)
+        {
+            if (_sources is not { Count: > 0 }) return;
+
+            var removed = await _repository.Delete(source);
+            if (removed is not null) _sources.Remove(removed);
+        }
+    }
+}
