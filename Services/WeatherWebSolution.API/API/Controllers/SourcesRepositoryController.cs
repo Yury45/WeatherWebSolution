a2409c642aea363f6b3fff1bde158cfdbd2bfe33
jiuc <zm.ptz.ru2010@yandex.ru>
2022-07-08 13:55:42 +0500

    Создан контроллер хранилища для источников данных

---
+++ Services/WeatherWebSolution.API/API/Controllers/SourcesRepositoryController.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using WeatherWebSolution.API.Controllers.Base;
+using WeatherWebSolution.DAL.Entities;
+using WeatherWebSolution.Domain.Base;
+using WeatherWebSolution.Intefaces.Base.Entities.Reposytories;
+
+namespace WeatherWebSolution.API.Controllers
+{
+    public class SourcesRepositoryController : MappedEntityController<DataSourceInfo, DataSource>
+    {
+        public SourcesRepositoryController(IRepository<DataSource> repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+    }
+}
