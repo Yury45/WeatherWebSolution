bd1bae48ecfc4aa8a7d44f14d9a5e746d5d1ca44
jiuc <zm.ptz.ru2010@yandex.ru>
2022-07-08 14:29:12 +0500

    Метод расширения для регистрации репозитория в Http клиенте

---
+++ UI/WeatherWebSolution.BlazorUI/Infrastructure/Extentions/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace WeatherWebSolution.BlazorUI.Infrastructure.Extentions
+{
+    internal static class ServiceExtensions
+    {
+        public static IHttpClientBuilder AddApi<IInterface, IClient>(this IServiceCollection services, string address)
+            where IInterface : class where IClient : class, IInterface=>
+            services.AddHttpClient<IInterface, IClient>((host, client) 
+                => client.BaseAddress = new(
+                    $"{host.GetRequiredService<IWebAssemblyHostEnvironment>().BaseAddress}{address}"));
+    }
+}
