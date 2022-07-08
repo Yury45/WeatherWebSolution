a2db9ab467ee4670e98ebcc425fcda18bb4fcc67
jiuc <zm.ptz.ru2010@yandex.ru>
2022-07-08 12:20:56 +0500

    Модели данных для слоя бизнес-логики

---
+++ Common/WeatherWebSolution.Domain.Base/DataValueInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WeatherWebSolution.Intefaces.Base.Entities;
+
+namespace WeatherWebSolution.Domain.Base
+{
+    internal class DataValueInfo : IEntity
+    {
+        public int Id { get; }
+
+        public DateTimeOffset Time { get; set; }
+
+        public string Value { get; set; }
+
+        bool IsFaultead { get; set; }
+    }
+}
